28dde420621eacb4a7f529dccd671a611e71ec6c
Adis Veletanlic <adis.veletanlic@gmail.com>
2022-10-09 14:53:02 +0200

    Add seminar 3

---
+++ OS_Seminar_3/java/src/memory/MemoryManager.java
@@ -0,0 +1,132 @@
+package src.memory;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MemoryManager {
+
+	private int myNumberOfPages;
+	private int myPageSize; // In bytes
+	private int myNumberOfFrames;
+	private int[] myPageTable; // -1 if page is not in physical memory
+	private byte[] myRAM; // physical memory RAM
+	private RandomAccessFile myPageFile;
+	private int myNextFreeFramePosition = 0;
+	private int myNumberOfpageFaults = 0;
+	private int myPageReplacementAlgorithm = 0;
+
+	public MemoryManager(int numberOfPages, int pageSize, int numberOfFrames, String pageFile,
+			int pageReplacementAlgorithm) {
+
+		myNumberOfPages = numberOfPages;
+		myPageSize = pageSize;
+		myNumberOfFrames = numberOfFrames;
+		myPageReplacementAlgorithm = pageReplacementAlgorithm;
+
+		initPageTable();
+		myRAM = new byte[myNumberOfFrames * myPageSize];
+
+		try {
+
+			myPageFile = new RandomAccessFile(pageFile, "r");
+
+		} catch (FileNotFoundException ex) {
+			System.out.println("Can't open page file: " + ex.getMessage());
+		}
+	}
+
+	private void initPageTable() {
+		myPageTable = new int[myNumberOfPages];
+		for (int n = 0; n < myNumberOfPages; n++) {
+			myPageTable[n] = -1;
+		}
+	}
+
+	public byte readFromMemory(int logicalAddress) {
+		int pageNumber = getPageNumber(logicalAddress);
+		int offset = getPageOffset(logicalAddress);
+		if (myPageTable[pageNumber] == -1) {
+			pageFault(pageNumber);
+		}
+		int frame = myPageTable[pageNumber];
+
+		int physicalAddress = frame * myPageSize + offset;
+		byte data = myRAM[physicalAddress];
+
+		System.out.print("Virtual address: " + logicalAddress);
+		System.out.print(" Physical address: " + physicalAddress);
+		System.out.println(" Value: " + data);
+		return data;
+	}
+
+	private int getPageNumber(int logicalAddress) {
+		Arrays.sort(myPageTable);
+        int res = Arrays.binarySearch(myPageTable, logicalAddress);
+ 
+        return 0;
+	}
+
+	private int getPageOffset(int logicalAddress) {
+		// Implement by student in task one
+		return 0;
+	}
+
+	private void pageFault(int pageNumber) {
+		if (myPageReplacementAlgorithm == Seminar3.NO_PAGE_REPLACEMENT)
+			handlePageFault(pageNumber);
+
+		if (myPageReplacementAlgorithm == Seminar3.FIFO_PAGE_REPLACEMENT)
+			handlePageFaultFIFO(pageNumber);
+
+		if (myPageReplacementAlgorithm == Seminar3.LRU_PAGE_REPLACEMENT)
+			handlePageFaultLRU(pageNumber);
+
+		readFromPageFileToMemory(pageNumber);
+	}
+
+	private void readFromPageFileToMemory(int pageNumber) {
+		try {
+			int frame = myPageTable[pageNumber];
+			myPageFile.seek(pageNumber * myPageSize);
+			for (int b = 0; b < myPageSize; b++)
+				myRAM[frame * myPageSize + b] = myPageFile.readByte();
+		} catch (IOException ex) {
+
+		}
+	}
+
+	public int getNumberOfPageFaults() {
+		return myNumberOfpageFaults;
+	}
+
+	private void handlePageFault(int pageNumber) {
+		/// If the pageNumber is not found in the current page table
+		/// we will try to iterate over the page table and find an empty
+		/// place for it.
+		for (int i = 0; i < myPageTable.length; i++) {
+			if(myPageTable[i] == -1) {
+				myPageTable[i] = pageNumber;
+			}
+		}
+
+	}
+
+	private void handlePageFaultFIFO(int pageNumber) {
+		// Implement by student in task two
+		// this solution allows different size of physical and logical memory
+		// page replacement using FIFO
+		// Note depending on your solution, you might need to change parts of the
+		// supplied code, this is allowed.
+	}
+
+	private void handlePageFaultLRU(int pageNumber) {
+		// Implement by student in task three
+		// this solution allows different size of physical and logical memory
+		// page replacement using LRU
+		// Note depending on your solution, you might need to change parts of the
+		// supplied code, this is allowed.
+	}
+}
